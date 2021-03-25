1dd97f66714bad20e185ea18f6f819f035ac9607
Zach Frohardt <zfrohardt@gmail.com>
2021-03-24 18:48:05 -0700

    Assignment completed

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function increment(x) {
+    return x + 1;
+}
+
+function decrement(x) {
+    return x - 1;
+}
+
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
